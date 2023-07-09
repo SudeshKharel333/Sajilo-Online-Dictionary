dd13933967deebd9cebfa3ff7c299403ca92d131
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-07-09 23:40:23 +0545

    removed background color and put firstname and last name side by side

--- signup.php
+++ signup.php
@@ -1,58 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Registration form</title>
+  <title>Register</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <style>
-      body
-      {
-          background-color: #b0e0e6 ;
-      }
-  </style>
+  <link rel="stylesheet" href="./css/signup.css">
 </head>
 <body>
 
-<div class="container mt-3">
-  <h2>Registration form</h2>
-  <form action="signup.php" method="post">
-    <div class="mb-3 mt-3">
-      <label for="FirstName">FirstName</label>
-      <input type="text" class="form-control" id="firstname" placeholder="Enter your Firstname" name="firstname">
-    </div>
-    <div class="mb-3">
-      <label for="LastName">LastName</label>
-      <input type="text" class="form-control" id="lastname" placeholder="Enter your Lastname" name="lastname">
-    </div>
-    <div class="mb-3">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-    </div>
-    <div class="mb-3">
-    <label for="username">Username:</label>
-      <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
-    </div>
-    
-    <div class="mb-3">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
-    </div>
-    <div class="mb-3">
-      <input type="radio" id="male" name="gender" value="male">
-      <label for="male">Male</label><br>
-      <input type="radio" id="female" name="gender" value="female">
-      <label for="female">Female</label><br>
-      <input type="radio" id="other" name="gender" value="other">
-      <label for="other">Other</label><br>
-      <label for="age">Age:</label>
-      <input type="number" id="age" name="age" min="1" max="120" required><br>
-      <label for="dob">Date of Birth:</label>
-      <input type="date" id="dob" name="dob" required><br>
-    </div>
-    <button type="submit" class="btn btn-primary" id="submitBtn">Submit</button>
-  </form>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 offset-md-2 signup-form">
+      <h2>Register</h2>
+      <form action="signup.php" method="post">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="mb-3">
+            <label for="FirstName">First Name</label>
+            <input type="text" class="form-control" id="firstname"  name="firstname">
+          </div>
+          </div>
+          <div class="col-md-6">
+            <div class="mb-3">
+            <label for="LastName">Last Name</label>
+            <input type="text" class="form-control" id="lastname" name="lastname">
+        </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="email">Email:</label>
+          <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="mb-3">
+        <label for="username">Username:</label>
+          <input type="text" class="form-control" id="username" name="username">
+        </div>
+        
+        <div class="mb-3">
+          <label for="pwd">Password:</label>
+          <input type="password" class="form-control" id="pwd" name="pswd" required>
+        </div>
+        <div class="mb-3">
+          <label for="age">Age:</label>
+          <input type="number" class="form-control" id="age" name="age">
+        </div>
+        <div class="mb-3">
+          <input type="radio" id="male" name="gender" value="male">
+          <label for="male">Male</label>
+          <input type="radio" id="female" name="gender" value="female">
+          <label for="female">Female</label>
+          <input type="radio" id="other" name="gender" value="other">
+          <label for="other">Other</label>
+        </div>
+        <button type="submit" class="btn btn-primary" id="submitBtn">Submit</button>
+      </form>
+  </div>
 </div>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
