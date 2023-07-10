efdd16a49b9de7e9b249d0033e1fb931470e5376
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-07-10 13:40:01 +0545

    added bootstrap CDN and items into recent search

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sajilo Online Dictionary</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="./css/index.css">
     <!--google font link -->
     <link rel="icon" type="image/x-icon" href="./images/1.png">
@@ -38,7 +40,13 @@
                 <p id="resultSection" ></p>
             </div>
             <div class="col-lg-4 col-md-8">
-                Recent Searches
+                <h3>Recent Searches</h3>
+                <p>Apple: <a href="#"></a><span class="glyphicon glyphicon-trash"></span></p>    
+                <p>ball <a href="#"></a><span class="glyphicon glyphicon-trash"></span></p>
+                <p>cat <a href="#"></a><span class="glyphicon glyphicon-trash"></span></p>
+                <p>dog<a href="#"></a><span class="glyphicon glyphicon-trash"></span></p>
+                <p>elephant <a href="#"></a><span class="glyphicon glyphicon-trash"></span></p>
+                
             </div>
         </div>
     </div>
