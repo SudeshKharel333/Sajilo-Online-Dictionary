b45fad6be9be8511952fdf4d9e0ed2b44416b45d
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-07-17 22:32:38 +0545

    modified footer

--- index.php
+++ index.php
@@ -81,7 +81,6 @@
                 ?>
             </div>
             <?php } ?>
-
         </div>
     </div>
 
@@ -90,8 +89,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
-<footer>
-    © 2023 Sajilo Online Dictionary. All rights reserved.
-</footer>
+<?php include './includes/footer.php'; ?>
 
 </html>
