9798e53ed00ecac3d5183707949625e8c6108e5a
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-07-23 11:03:01 +0545

    added header include statement

--- about.php
+++ about.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,28 +11,16 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong">
      <!--google font link -->
-     <link rel="icon" type="image/x-icon" href="sajilo1.png">
-    <link rel="stylesheet" href="AboutStyle.css">
+     <link rel="icon" type="image/x-icon" href="./images/xs_logo.png">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/about-styles.css">
 </head>
 <body>
-    <nav>
-        <div class="logo">
-            <img src="1.png" width="200px" height="150px">
-        
-        <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="signup.php"> Sign Up</a></li>
-        </ul>
-        </div>
-    </nav> 
-        <img src="sajilo1.png" class="center">
+    <?php include './includes/header.php'; ?>
         <div class="container pt-5">
             <h1 style="font-family:'Sofia', monospace ;font-size: 55px;">Sajilo Online Dictionary</h1>
         </div>
         <div class="container pt-5 jumbotron well"> 
-            <h2>About Sajilo Online Dictionary</h2>
             <p>
                 Sajilo Online Dictionary is an easy-to-use and comprehensive online dictionary that provides accurate meanings, synonyms, antonyms, pronunciations, and more for various words. It aims to assist users in expanding their vocabulary and understanding the English language better.
             </p>
