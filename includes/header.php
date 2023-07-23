1ddd7cf952cfee67d2211df32a3a4a26d0b86c49
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-07-23 11:09:49 +0545

    added header.php

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<?php
+    echo "
+    <nav>
+        <img src='./images/logo.png' height='100px'>
+        <span id='site-name'><b>Sajilo Online Dictionary</b></span>
+        <ul>
+                   
+            <li><a href='index.php'>Home</a></li>
+            <li><a href='about.php'>About</a></li>";
+            if(!isset($_SESSION['user'])){ 
+                echo "<li><a href='login.php'>Login</a></li>
+                <li><a href='signup.php'> Sign Up</a></li>";
+            }
+            else {
+                echo "
+                <li><a href='logout.php'> LogOut</a></li>";
+            }
+        echo "</ul> </nav>";
+?>
