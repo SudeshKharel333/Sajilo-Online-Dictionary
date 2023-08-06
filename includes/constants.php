aefc431a77144f232627a9fc50358a420034b7a7
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-08-06 23:23:52 +0545

    created  constant.php

---
+++ includes/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbHost = "localhost"; // replace with your host
+$dbUsername = "root"; // replace with your database username
+$dbPassword = ""; // replace with your database password
+$dbName = "sajilo_online_dictionary"; // replace with your database name
+
+
+?>
