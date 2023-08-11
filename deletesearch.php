58474ae17a877d9078b83378fcdaf1ac0e98d18a
Sudesh Kharel <Sudeshkharel15@gmail.com>
2023-08-12 00:38:15 +0545

    changed search to word in all references

--- deletesearch.php
+++ deletesearch.php
@@ -1,9 +1,9 @@
 <?php
 // deletesearch.php?searchid=5
     session_start();
-    if($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['searchid']) && $_GET['searchid'])
+    if($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['wordid']) && $_GET['wordid'])
     {
-        $searchid = $_GET['searchid'];
+        $wordid = $_GET['wordid'];
         $userId = $_SESSION['userid'];
         
         include './includes/constants.php'; 
@@ -13,7 +13,7 @@
         {
             die("Connection failed: " . $conn->connect_error);
         }
-        $worddeleteQuery = "DELETE FROM search  WHERE searchid = $searchid";
+        $worddeleteQuery = "DELETE FROM word  WHERE wordid = $wordid";
         $conn->query($worddeleteQuery);
         header("Location: index.php");
         
